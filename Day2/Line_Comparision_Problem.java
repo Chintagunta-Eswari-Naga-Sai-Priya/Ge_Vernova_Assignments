2452c55d8682ec9e7ba5bee7468ffbbf1ccb9f6b
Chintagunta Eswari Naga Sai Priya <121191802+Chintagunta-Eswari-Naga-Sai-Priya@users.noreply.github.com>
2024-12-07 16:34:17 +0530

    Delete Day2 directory

--- Day2/Line_Comparision_Problem.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class LineComparison {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
-        // Placeholder for further logic
-    }
-}

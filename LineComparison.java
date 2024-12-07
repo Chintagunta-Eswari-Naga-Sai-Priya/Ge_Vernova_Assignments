ac26fc5fa359d830ae7a8ef307f42a6402a2a829
Chintagunta_Eswari_Naga_Sai_Priya <priyachintagunta3@gmail.com>
2024-12-07 17:21:26 +0530

    Initial commit: Display welcome message

---
+++ LineComparison.java
@@ -0,0 +1,8 @@
+package Day2;
+
+// LineComparison.java
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+    }
+}

d975ad27bc1779c4248db1b2a3220452d72e70b6
Chintagunta_Eswari_Naga_Sai_Priya <priyachintagunta3@gmail.com>
2024-12-09 15:30:28 +0530

    Implement UC6: Manage multiple contacts in AddressBook

--- src/Day6/AddressBook.java
+++ src/Day6/AddressBook.java
@@ -1,20 +1,44 @@
-// AddressBookMain.java
 package Day6;
-import Day6.Contact;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-    private static ArrayList<Contact> contacts = new ArrayList<>();
+    private ArrayList<Contact> contacts = new ArrayList<>();
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
-        addContact();
+    public void manageContacts() {
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
+
+        while (isRunning) {
+            System.out.println("\nAddress Book Menu:");
+            System.out.println("1. Add Contact");
+            System.out.println("2. View All Contacts");
+            System.out.println("3. Exit");
+            System.out.print("Choose an option: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    addContact(scanner);
+                    break;
+                case 2:
+                    viewContacts();
+                    break;
+                case 3:
+                    isRunning = false;
+                    System.out.println("Exiting Address Book. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+        scanner.close();
     }
 
-    private static void addContact() {
-        Scanner scanner = new Scanner(System.in);
+    private void addContact(Scanner scanner) {
         System.out.println("Enter Contact Details:");
 
         System.out.print("First Name: ");
@@ -37,5 +61,16 @@ public class AddressBook {
         Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
         contacts.add(contact);
         System.out.println("Contact Added: " + contact);
+    }
+
+    private void viewContacts() {
+        if (contacts.isEmpty()) {
+            System.out.println("No contacts in the Address Book.");
+        } else {
+            System.out.println("Contacts in Address Book:");
+            for (Contact contact : contacts) {
+                System.out.println(contact);
+            }
+        }
     }
 }

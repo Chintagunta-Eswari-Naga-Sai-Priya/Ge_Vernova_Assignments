53ca6af6838ab8d6eb7b22a10374ad40eb7d1155
Chintagunta_Eswari_Naga_Sai_Priya <priyachintagunta3@gmail.com>
2025-01-03 22:09:06 +0530

    solved the error

--- src/Day6/EmployeeWages/EmployeeWageComputation.java
+++ src/Day6/EmployeeWages/EmployeeWageComputation.java
@@ -1,16 +1,42 @@
 package Day6.EmployeeWages;
 
-import java.util.HashMap;
-import IEmployeeWage.java;
-public class EmployeeWageComputation {
-    private HashMap<String, Integer> companyWageMap = new HashMap<>();
+import java.util.ArrayList;
 
+public class EmployeeWageComputation implements IEmployeeWage {
+    private ArrayList<CompanyEmpWage> companyWageList = new ArrayList<>();
+
+    @Override
     public void addCompany(String companyName, int wagePerHour, int fullDayHour, int maxWorkingDays, int maxWorkingHours) {
         int totalWage = computeEmployeeWage(companyName, wagePerHour, fullDayHour, maxWorkingDays, maxWorkingHours);
-        companyWageMap.put(companyName, totalWage);
+        companyWageList.add(new CompanyEmpWage(companyName, totalWage));
     }
 
-    public int getTotalWageByCompany(String companyName) {
-        return companyWageMap.getOrDefault(companyName, 0);
+    @Override
+    public void computeAllEmployeeWages() {
+        for (CompanyEmpWage company : companyWageList) {
+            System.out.println(company);
+        }
+    }
+
+    private int computeEmployeeWage(String companyName, int wagePerHour, int fullDayHour, int maxWorkingDays, int maxWorkingHours) {
+        int totalWage = 0;
+        int totalHours = 0;
+        int days = 0;
+
+        while (days < maxWorkingDays && totalHours < maxWorkingHours) {
+            int hoursWorked = (int) (Math.random() * (fullDayHour + 1));
+            totalHours += hoursWorked;
+            totalWage += hoursWorked * wagePerHour;
+            days++;
+        }
+        return totalWage;
+    }
+
+    public static void main(String[] args) {
+        EmployeeWageComputation computation = new EmployeeWageComputation();
+        computation.addCompany("CompanyA", 20, 8, 20, 100);
+        computation.addCompany("CompanyB", 25, 8, 22, 120);
+
+        computation.computeAllEmployeeWages();
     }
 }
--- src/Day6/EmployeeWages/IEmployeeWage.java
+++ src/Day6/EmployeeWages/IEmployeeWage.java
@@ -1,25 +1,6 @@
 package Day6.EmployeeWages;
 
-import java.util.ArrayList;
-
-interface IEmployeeWage {
+public interface IEmployeeWage {
     void addCompany(String companyName, int wagePerHour, int fullDayHour, int maxWorkingDays, int maxWorkingHours);
     void computeAllEmployeeWages();
-}
-
-public class EmployeeWageComputation implements IEmployeeWage {
-    private ArrayList<CompanyEmpWage> companyWageList = new ArrayList<>();
-
-    @Override
-    public void addCompany(String companyName, int wagePerHour, int fullDayHour, int maxWorkingDays, int maxWorkingHours) {
-        int totalWage = computeEmployeeWage(companyName, wagePerHour, fullDayHour, maxWorkingDays, maxWorkingHours);
-        companyWageList.add(new CompanyEmpWage(companyName, totalWage));
-    }
-
-    @Override
-    public void computeAllEmployeeWages() {
-        for (CompanyEmpWage company : companyWageList) {
-            System.out.println(company);
-        }
-    }
 }

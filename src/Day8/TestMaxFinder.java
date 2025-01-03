c977c4b52e6f3b7e8de5e636922a827910ce8eb7
Chintagunta_Eswari_Naga_Sai_Priya <priyachintagunta3@gmail.com>
2025-01-03 23:42:54 +0530

    Add test cases for strings

--- src/Day8/TestMaxFinder.java
+++ src/Day8/TestMaxFinder.java
@@ -1,11 +1,10 @@
 package Day8;
-
 // src/TestMaxFinder.java
-public class TestMaxFinder // src/TestMaxFinder.java
 public class TestMaxFinder {
     public static void main(String[] args) {
         testIntegerMaximum();
         testFloatMaximum();
+        testStringMaximum();
     }
 
     public static void testIntegerMaximum() {
@@ -29,5 +28,15 @@ public class TestMaxFinder {
         System.out.println("Test Case 2.3: Max at 3rd Position");
         System.out.println("Max: " + MaxFinder.testMaximum(1.1f, 4.4f, 9.9f)); // Max at 3rd position
     }
-}
 
+    public static void testStringMaximum() {
+        System.out.println("Test Case 3.1: Max at 1st Position");
+        System.out.println("Max: " + MaxFinder.testMaximum("Apple", "Peach", "Banana")); // Max at 1st position
+
+        System.out.println("Test Case 3.2: Max at 2nd Position");
+        System.out.println("Max: " + MaxFinder.testMaximum("Peach", "Zebra", "Banana")); // Max at 2nd position
+
+        System.out.println("Test Case 3.3: Max at 3rd Position");
+        System.out.println("Max: " + MaxFinder.testMaximum("Grapes", "Banana", "Orange")); // Max at 3rd position
+    }
+}

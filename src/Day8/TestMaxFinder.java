8f2a717667fe2e0e092dc55358f2c1c36ca68ac2
Chintagunta_Eswari_Naga_Sai_Priya <priyachintagunta3@gmail.com>
2025-01-03 23:40:35 +0530

    Add test cases for integers

--- src/Day8/TestMaxFinder.java
+++ src/Day8/TestMaxFinder.java
@@ -1,4 +1,19 @@
 package Day8;
 
+// src/TestMaxFinder.java
 public class TestMaxFinder {
+    public static void main(String[] args) {
+        testIntegerMaximum();
+    }
+
+    public static void testIntegerMaximum() {
+        System.out.println("Test Case 1.1: Max at 1st Position");
+        System.out.println("Max: " + MaxFinder.testMaximum(5, 3, 2)); // Max at 1st position
+
+        System.out.println("Test Case 1.2: Max at 2nd Position");
+        System.out.println("Max: " + MaxFinder.testMaximum(3, 7, 2)); // Max at 2nd position
+
+        System.out.println("Test Case 1.3: Max at 3rd Position");
+        System.out.println("Max: " + MaxFinder.testMaximum(1, 4, 9)); // Max at 3rd position
+    }
 }

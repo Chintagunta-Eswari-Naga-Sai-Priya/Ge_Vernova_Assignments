93177454107dcf756c3f31929f9e2eb799b35aa8
Chintagunta_Eswari_Naga_Sai_Priya <priyachintagunta3@gmail.com>
2025-01-05 23:26:28 +0530

    Implemented first name validation

--- src/Day12/main/java/User.java
+++ src/Day12/main/java/User.java
@@ -7,5 +7,17 @@ public class User {
     private String mobile;
     private String password;
 
+    public User(String firstName, String lastName, String email, String mobile, String password) {
+        if (!isValidFirstName(firstName)) {
+            throw new IllegalArgumentException("Invalid First Name");
+        }
+        // Other validations to be added later
+    }
+
+    private boolean isValidFirstName(String firstName) {
+        return firstName != null && firstName.length() >= 3 && Character.isUpperCase(firstName.charAt(0));
+    }
+
+
     // Constructor and validations to be added later
 }
